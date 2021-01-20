c0b1c95fd026adb9a8fc4cb8da429a46f0c18e1f
Malek Kefi <melek.kefi@sesame.com.tn>
2021-01-20 20:43:26 +0100

    first commit

--- src/Photo.js
+++ src/Photo.js
@@ -4,12 +4,17 @@ class Photo extends Component {
         return(
             <div>
                 <h2>Welcome to my App</h2>
-                <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" alt=""></img>
-                <img src="/images/30.jpg" alt="" class="center"></img>
-                <img src="/images/80.jpg" alt=""></img>
+                <img src="https://cache.marieclaire.fr/data/photo/w1536_ci/53/printemps-cerisier1.webp" alt="" width="500" height="350"></img>
+                <img src="https://cdn.radiofrance.fr/s3/cruiser-production/2020/03/57bd5edf-104b-4516-a07f-7063a11b9953/870x489_gettyimages-986913308-170667a.webp"
+           
+                alt=""width="450" height="350"></img>
+                <img src="https://viago.ca/wp-content/uploads/2019/03/Netherlands-Keukenhof-768x432.jpg"
+           
+           alt=""width="450" height="350"></img>
             </div>
         );
     }
 
 }
 export default Photo;
+
